97f2ad9c0814a661b0baa05b22b23449f7678aeb
Wes Gunning <wesgunning@gmail.com>
2022-12-17 09:44:20 -0500

    Build calculator functions and modify spec.js to run functions against tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = array[0];
+	for (let i = 1; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {
+    return num = 1;
+  }
+  else{
+    let total = num;
+    while (num > 1) {
+      total *= (num - 1);
+      num--;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
