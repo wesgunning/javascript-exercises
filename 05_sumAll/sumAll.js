89ba75b30b8cd3c1534ca88c5026528c1b73feb0
Wes Gunning <wesgunning@gmail.com>
2022-12-16 20:30:16 -0500

    Modify sumAll.js and  spec.js to run code against skipped tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,20 @@
 const sumAll = function(firstNum, lastNum) {
     let sum = 0;
-    for (let i = firstNum; i <= lastNum; i++) {
-        sum += i;
+    if (firstNum < 0 || lastNum < 0 || (typeof(firstNum) != 'number') || (typeof(lastNum) != 'number')) {
+        return 'ERROR'
     }
-    return sum;
+    else if (firstNum > lastNum) {
+        for (let i = firstNum; i >= lastNum; i--) {
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        for (let i = firstNum; i <= lastNum; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
