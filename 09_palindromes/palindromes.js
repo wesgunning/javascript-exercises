1669a0404b06da8462cc3b1c8090e683ada1fdae
Wes Gunning <wesgunning@gmail.com>
2022-12-17 14:08:46 -0500

    Write function to palindrome.js and modify spec.js to run code against tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function(word) {
+    // Rebuild word without punctuation or spaces
+    let string = '';
+    word = word.toLowerCase();
+    for (let i = 0; i < word.length; i++) {
+        if (word[i].toUpperCase() != word[i]) {
+            string += word[i];
+        }
+    }
 
+    // Build reverse of word & assign to variable
+    let reverse = '';
+
+    for (let i = (string.length - 1); i >= 0; i--) {
+        reverse += string[i];
+    }
+
+    // Compare reverse to string variable
+    for (let i = 0; i < string.length; i++) {
+        if (reverse[i] != string[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
