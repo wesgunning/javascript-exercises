5893257c805f13217f8501590e36265c5675f46f
Wes Gunning <wesgunning@gmail.com>
2022-12-17 14:38:50 -0500

    Write function in fibonacci.js and modify spec.js to run code against tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // Check for negative number
+    if (num < 0) {
+        return 'OOPS';
+    }
 
+    // Create array of numbers up to num
+    let array = [1,1];
+    for (i = 2; i < num; i++) {
+        array[i] = array[i-1] + array[i-2];
+    }
+    return array[(num - 1)];
+    // Sum array number from num-2 & num-1 and return
 };
 
 // Do not edit below this line
