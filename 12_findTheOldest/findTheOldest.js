c13a31e511321687b66681713ddc228ffe2ce919
Wes Gunning <wesgunning@gmail.com>
2022-12-27 16:17:46 -0500

    Modify js and spec.js files to complete exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, thisPerson) => {
+        const oldestAge = findAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = findAge(thisPerson.yearOfBirth, thisPerson.yearOfDeath);
+        if (oldestAge < currentAge) {
+            return thisPerson;
+        }
+        else {
+            return oldest;
+        }
+    })
 };
+
+const findAge = function(birth, death) {
+    if(!death) {
+        const today = new Date();
+        const year = today.getFullYear();
+        return year - birth;
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
